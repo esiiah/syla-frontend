dfa0a3c5b27b3436b44d0d08dc939d79af180196
Esiah Kapinga <esiahkapinga3@gmail.com>
2025-08-21 15:35:43 +0200

    Update FileUpload.js

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -13,7 +13,7 @@ function FileUpload() {
     formData.append("file", file);
 
     try {
-      const response = await fetch("https://syla-bankend.onrender.com", {
+      const response = await fetch("https://syla-backend.onrender.com/upload", {
         method: "POST",
         body: formData,
       });
