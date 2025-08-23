4fa15769d35758d799b2c0a90c769f9c73e9aa99
Esiah Kapinga <esiahkapinga3@gmail.com>
2025-08-23 16:00:24 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./App.css";
 import App from "./App";
 import "./index.css";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration"; // <-- added
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -10,3 +11,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// Register the service worker for offline support
+serviceWorkerRegistration.register();
