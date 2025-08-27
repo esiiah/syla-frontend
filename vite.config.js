84f478cb29fd3088de6a998c245e0480bf01e790
Esiah Kapinga <esiahkapinga3@gmail.com>
2025-08-27 12:48:07 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   esbuild: {
-    loader: "jsx",
-    include: /src\/.*\.js$/, // process .js files in src/ as JSX
-  },
-  build: {
-    outDir: 'dist',       // Vite default, matches Render publish directory
-    emptyOutDir: true,    // Clear dist/ before building
-    sourcemap: false,     // Optional: remove source maps in production
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'), // optional shortcut for imports
-    },
+    loader: 'jsx',
+    include: /src\/.*\.js$/, // treat .js files in src/ as JSX
   },
 })
